3278307afaeae4eb3c935f5e1d8e4e6681e19889
keyring88 <keyring88@users.noreply.github.com>
2022-05-20 19:49:07 +0000

    ✅ Ready to clone and code.

--- .github/rename_project.sh
+++ .github/rename_project.sh
@@ -16,10 +16,10 @@ echo "Description: $description";
 
 echo "Renaming project..."
 
-original_author="author_name"
-original_name="project_name"
-original_urlname="project_urlname"
-original_description="project_description"
+original_author="keyring88"
+original_name="flask"
+original_urlname="flask"
+original_description="Awesome flask created by keyring88"
 # for filename in $(find . -name "*.*") 
 for filename in $(git ls-files) 
 do
@@ -30,7 +30,7 @@ do
     echo "Renamed $filename"
 done
 
-mv project_name $name
+mv flask $name
 
 # This command runs only once on GHA!
 rm -rf .github/template.yml
